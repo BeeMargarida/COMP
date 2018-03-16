95dc2dd21fa92a6a10c01e911fbf954484c1acee
Pedro Daniel Reis <up201506046@fe.up.pt>
2018-03-16 19:15:50 +0000

    Changed computers

--- Project/src/ASTWhile.java
+++ Project/src/ASTWhile.java
@@ -2,12 +2,19 @@
 /* JavaCCOptions:MULTI=true,NODE_USES_PARSER=false,VISITOR=false,TRACK_TOKENS=false,NODE_PREFIX=AST,NODE_EXTENDS=,NODE_FACTORY=,SUPPORT_CLASS_VISIBILITY_PUBLIC=true */
 public
 class ASTWhile extends SimpleNode {
+
+  public String exprTest; 
+
   public ASTWhile(int id) {
     super(id);
   }
 
   public ASTWhile(Parser p, int id) {
     super(p, id);
+  }
+ 
+  public String toString(String prefix) {
+  	return prefix + "Estou num while, fam, com exprTest a " + exprTest;
   }
 
 }
