4d5bd2a0a5cb6f0a18fabb18ba37348fceb18766
Pedro Daniel Reis <up201506046@fe.up.pt>
2018-04-14 15:37:48 +0100

    Another high priority fix

--- Project/src/ASTArrayElement.java
+++ Project/src/ASTArrayElement.java
@@ -2,12 +2,18 @@
 /* JavaCCOptions:MULTI=true,NODE_USES_PARSER=false,VISITOR=false,TRACK_TOKENS=false,NODE_PREFIX=AST,NODE_EXTENDS=,NODE_FACTORY=,SUPPORT_CLASS_VISIBILITY_PUBLIC=true */
 public
 class ASTArrayElement extends SimpleNode {
+  public String content; 
+
   public ASTArrayElement(int id) {
     super(id);
   }
 
   public ASTArrayElement(Parser p, int id) {
     super(p, id);
+  }
+
+  public String toString(String prefix) {
+    return prefix + content + "[]";
   }
 
 }
