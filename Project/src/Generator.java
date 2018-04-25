35796730d119bb1cbd743c821b86ddfd925c62c3
beemargarida <up201505505@gcloud.fe.up.pt>
2018-04-25 17:07:45 +0100

    started call and assign

--- Project/src/Generator.java
+++ Project/src/Generator.java
@@ -1,7 +1,7 @@
 public class Generator {
 
     private Sampler sampler;
-    private String currentFunctionName;
+    private String moduleName;
 
     public Generator(Sampler sampler) {
         this.sampler = sampler;
@@ -14,11 +14,7 @@ public class Generator {
             visit((ASTModule) node);
         }
         else if(node.toString().equals("Function")){
-            System.out.println("Function");
             visit((ASTFunction) node);
-        }
-        else if(node instanceof ASTVar) {
-
         }
 
         return null;
@@ -29,6 +25,7 @@ public class Generator {
     public Object visit(ASTModule node) {
 
         // prints Module yasmin code
+        this.moduleName = node.name;
         sampler.startModule(node.name);
 
         // visits Module nodes
@@ -44,6 +41,7 @@ public class Generator {
     // Function Node
     public Object visit(ASTFunction node) {
         int childN = 0;
+        System.out.println("Function: " + node.functionName);
         if(node.functionName.equals("main")){
             // if the function is the main one
             sampler.functionBegin(node.functionName, null);
@@ -51,7 +49,6 @@ public class Generator {
         else {
             // If there is no Parameters (Var)
             if(node.jjtGetChild(childN).jjtGetNumChildren() == 0){
-                System.out.println("No Params");
                 sampler.functionBegin(node.functionName, null);
             }
             else {
@@ -59,15 +56,30 @@ public class Generator {
                 String[] vars = (String[]) visit((ASTVarList) node.jjtGetChild(0)); 
                 sampler.functionBegin(node.functionName, vars);
             }
-            childN++; 
         }
+        childN++; 
 
+        // Print stack and locals values
+        // TODO
+
+        // Checks the other children of the function
+        for(int i = childN; i < node.jjtGetNumChildren(); i++){
+            checkFunctionChildren((SimpleNode) node.jjtGetChild(i));
+        }
        
         // prints yasmin Function endline
         sampler.functionEnd();
 
-        System.out.println("Function: " + node.functionName);
         return null;
+    }
+
+    public void checkFunctionChildren(SimpleNode node){
+        if(node.toString().equals("Call")){
+            visit((ASTCall) node);
+        }
+        else if(node.toString().equals("Assign")){
+            visit((ASTAssign) node);
+        }
     }
 
     // gets all the types of vars
@@ -86,6 +98,32 @@ public class Generator {
         System.out.println("Node: " + node.getValue() + " TYPE: " + node.getType());
         return node.getType();
     }
+
+    public Object visit(ASTCall node) {
+        SimpleNode varList = (SimpleNode) node.jjtGetChild(0);
+
+        for(int i = 0; i < node.jjtGetNumChildren(); i++){
+            System.out.println("CHILDREN   AH: " + node.toString());
+        }
+
+        for(int i = 0; i < varList.jjtGetNumChildren(); i++){
+            ASTArgument arg = (ASTArgument) varList.jjtGetChild(i);
+            System.out.println("CHILDREN: " + arg.content);
+            for(int a = 0; a < arg.jjtGetNumChildren(); a++){
+                System.out.println("CHILDREN 2: " + arg.jjtGetChild(a).toString());
+            }
+        }
+        //sampler.print("invokestatic " + this.moduleName+ "/" + node.calledFunction); //missing type of arguments and return
+
+        return null;
+    }
+
+    public Object visit(ASTAssign node) {
+        System.out.println("ASSIGN");
+        return null;
+    }
+
+
 
     
 }
